aa32c84c3c0bab0cbc59ee39f86aa7207b20df4d
Sanjay4748 <sanjaythatipaka@gmai.com>
2024-07-19 08:32:55 +0530

    updated

--- frontend/taskmanager/src/App.js
+++ frontend/taskmanager/src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Header from "./Layout/Header";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Header/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/taskmanager/src/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../Layout/layout.module.css';
+import { Button } from 'react-bootstrap';
+
+function Header() {
+    return (
+        <div className={styles.navbar}>
+            <Button className={styles.buttonpart}>
+                Login
+            </Button>
+            <Button className={styles.buttonpart}>
+                Signup
+            </Button>
+        </div>
+    );
+}
+
+export default Header;
